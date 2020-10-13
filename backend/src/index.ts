409ab2f74c6ddc0829908560f4bdc1d700f8157a
espython <espython85@gmail.com>
2020-10-14 00:28:37 +0200

    rebuild the front-end

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,27 +1,27 @@
 import express from "express";
 import admin from "firebase-admin";
 
-import serviceAccount from "./clothing-db.json";
+// import serviceAccount from "./clothing-db.json";
 
-const defaultApp = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as any),
-  databaseURL: "https://clothing-db-2b748.firebaseio.com"
-});
-const db = defaultApp.firestore();
+// const defaultApp = admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount as any),
+//   databaseURL: "https://clothing-db-2b748.firebaseio.com"
+// });
+// const db = defaultApp.firestore();
 
 // rest of the code remains same
 const app = express();
 const PORT = 4000;
 app.get("/api", async (req, res) => {
   const users = [];
-  const usersQuery = (await db.collection("users").get()).docs;
-  console.log(
-    "users",
-    usersQuery.forEach(doc => {
-      console.log(doc.id, "=>", doc.data());
-      users.push(doc.data() as never);
-    })
-  );
+  // const usersQuery = (await db.collection("users").get()).docs;
+  // console.log(
+  //   "users",
+  //   usersQuery.forEach(doc => {
+  //     console.log(doc.id, "=>", doc.data());
+  //     users.push(doc.data() as never);
+  //   })
+  // );
   console.log("json", req.body);
   // console.log(defaultApp.)
   res.status(200).json({ users });
